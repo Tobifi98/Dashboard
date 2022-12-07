957951083365453bf7cfd437d46fb5f07db25c20
Tobias Fischer <andentobias@gmail.com>
2022-12-07 11:28:27 +0100

    Test component created

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
-import { FormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms'; //Ich weiß nicht warum der Import nicht erkannt wird... es funktioniert troztdem irgendwie...
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import { HelloWorldComponent } from '../test/hello-world/hello-world.component';
 
 
 @NgModule({
@@ -14,6 +15,6 @@ import { HomePageRoutingModule } from './home-routing.module';
     IonicModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage, HelloWorldComponent] //you need to import your component here to create a new component cou have to type "ionic g c" into the command line
 })
 export class HomePageModule {}
