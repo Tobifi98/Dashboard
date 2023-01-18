2b59593e1523c304048e6ce368dc8d6a33abc5f4
Snjezana Petrovic <snjezana.petrovic17@gmail.com>
2023-01-18 12:33:57 +0100

    doughnut WIP

--- src/app/doughnut-chart-component/doughnut-chart-component.component.ts
+++ src/app/doughnut-chart-component/doughnut-chart-component.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
+import { OnInit, Component, ElementRef, ViewChild  } from "@angular/core";
 import { Chart } from "chart.js";
 
 @Component({
@@ -6,37 +6,44 @@ import { Chart } from "chart.js";
   templateUrl: './doughnut-chart-component.component.html',
   styleUrls: ['./doughnut-chart-component.component.scss'],
 })
+
 export class DoughnutChartComponentComponent implements OnInit {
 
   @ViewChild("doughnutCanvas") doughnutCanvas: ElementRef;
 
-  private doughnutChart: Chart;
+  private doughnutChart: any;
+  public chartLabels               : any    = [];
+  public chartValues               : any    = ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"];
 
-  constructor() { 
+  constructor() {
+  }
+
+  defineChartData() {
   }
 
   ngOnInit() {
 
+    this.defineChartData();
+
     this.doughnutChart = new Chart(this.doughnutCanvas.nativeElement, {
       type: "doughnut",
       data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-        datasets: [
-          {
-            label: "# of Votes",
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-              "rgba(255, 99, 132, 0.2)",
-              "rgba(54, 162, 235, 0.2)",
-              "rgba(255, 206, 86, 0.2)",
-              "rgba(75, 192, 192, 0.2)",
-              "rgba(153, 102, 255, 0.2)",
-              "rgba(255, 159, 64, 0.2)"
-            ],
-            hoverBackgroundColor: ["#FF6384", "#36A2EB", "#FFCE56", "#FF6384", "#36A2EB", "#FFCE56"]
-          }
-        ]
+        labels: this.chartLabels,
+             datasets: [{
+                 label                 : 'Daily Technology usage',
+                 data                  : this.chartValues,
+                 backgroundColor: [
+                  "rgba(255, 99, 132, 0.2)",
+                  "rgba(54, 162, 235, 0.2)",
+                  "rgba(255, 206, 86, 0.2)",
+                  "rgba(75, 192, 192, 0.2)",
+                  "rgba(153, 102, 255, 0.2)",
+                  "rgba(255, 159, 64, 0.2)"
+                ],
+                 hoverBackgroundColor: ["#FF6384", "#36A2EB", "#FFCE56", "#FF6384", "#36A2EB", "#FFCE56"]
+             }]
       }
     });
   }
 }
+
