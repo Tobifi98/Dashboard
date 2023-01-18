6e15ca1d97c36c96b35da1adeea272aa593e1963
Robin Kunath <59505945+AnanasHead@users.noreply.github.com>
2023-01-18 12:08:09 +0100

    Dokumentation der wichtigen API Funktionen begonnen

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -10,23 +10,5 @@ import { Observable } from 'rxjs';
 })
 export class HomePage {
   constructor() {
-    getData();
-    async function getData() {
-      try {
-        const username = 'tester';
-        const password = 'training';
-        const headers = new Headers();
-        headers.append('Authorization', 'Basic ' + btoa(`${username}:${password}`));
-        headers.append('Content-Type', 'application/json');
-    
-        const response = await fetch('http://10.3.0.71:8080/mhubx-cc/module/juwi/action?page=Logic.Interface&name=getMeasurement&source=system&system_id=*&msm_id=*', {
-          headers: headers
-        });
-        const data = await response.json();
-        return data;
-      } catch (error) {
-        console.error(error);
-      }
-    }
   };
 }
